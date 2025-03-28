d785950563fd942aca040ae22fd36996c3d27e04
Trevor Bai <baijin2011@gmail.com>
2025-03-27 22:25:58 -0400

    Add 10 years average annual return rate for btc/doge

--- InvestmentCalculator/MainWindow.xaml.cs
+++ InvestmentCalculator/MainWindow.xaml.cs
@@ -28,28 +28,53 @@ namespace InvestmentCalculators
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            PopulateDogecoinAverageAnualReturnRateFromBorn();
-            PopulateBitcoinAverageAnualReturnRateFromBorn();
+            // Dogecoin
+            PopulateDogecoinAverageAnnualReturnRateFromBirth();
+            PopulateDogecoinAverageAnnualReturnRate10Years();
+
+            // Bitcoin
+            PopulateBitcoinAverageAnnualReturnRateFromBirth();
+            PopulateBitcoinAverageAnnualReturnRate10Years();
         }
 
-        private void PopulateDogecoinAverageAnualReturnRateFromBorn()
+        private void PopulateDogecoinAverageAnnualReturnRateFromBirth()
         {
             var dogecoinPriceAt2013Dec15th = 0.00056;
             var dogecoinPriceAt2025Mar25th = 0.1901;
             var yearSpan = 11.29;
             double dogecoinAverageAnnualReturnRateFromBirth = Calculators.CalculateAverageAnualReturnRate(
                 dogecoinPriceAt2013Dec15th, dogecoinPriceAt2025Mar25th, yearSpan);
-            CalculatedDogecoinAverageAnnualReturnRate.Content = $"{dogecoinAverageAnnualReturnRateFromBirth * 100:F2}%";
+            CalculatedDogecoinAverageAnnualReturnRateFromBirth.Content = $"{dogecoinAverageAnnualReturnRateFromBirth * 100:F2}%";
         }
 
-        private void PopulateBitcoinAverageAnualReturnRateFromBorn()
+        private void PopulateDogecoinAverageAnnualReturnRate10Years()
+        {
+            var dogecoinPriceAt2015Mar25th = 0.0001304;
+            var dogecoinPriceAt2025Mar25th = 0.1901;
+            var yearSpan = 10;
+            double dogecoinAverageAnnualReturnRateFromBirth = Calculators.CalculateAverageAnualReturnRate(
+                dogecoinPriceAt2015Mar25th, dogecoinPriceAt2025Mar25th, yearSpan);
+            CalculatedDogecoinAverageAnnualReturnRate10Years.Content = $"{dogecoinAverageAnnualReturnRateFromBirth * 100:F2}%";
+        }  
+
+        private void PopulateBitcoinAverageAnnualReturnRateFromBirth()
         {
             var bitcoinPriceAt2009Oct15th = 0.00099;
             var bitcoinPriceAt2025Mar26th = 86888.01;
             var yearSpan = 15.4757;
             double bitcoinAverageAnnualReturnRateFromBirth = Calculators.CalculateAverageAnualReturnRate(
                 bitcoinPriceAt2009Oct15th, bitcoinPriceAt2025Mar26th, yearSpan);
-            CalculatedBitcoinAverageAnnualReturnRate.Content = $"{bitcoinAverageAnnualReturnRateFromBirth * 100:F2}%";
+            CalculatedBitcoinAverageAnnualReturnRateFromBirth.Content = $"{bitcoinAverageAnnualReturnRateFromBirth * 100:F2}%";
+        }
+
+        private void PopulateBitcoinAverageAnnualReturnRate10Years()
+        {
+            var bitcoinPriceAt2015Mar28th = 252.74;
+            var bitcoinPriceAt2025Mar26th = 86888.01;
+            var yearSpan = 10;
+            double bitcoinAverageAnnualReturnRateFromBirth = Calculators.CalculateAverageAnualReturnRate(
+                bitcoinPriceAt2015Mar28th, bitcoinPriceAt2025Mar26th, yearSpan);
+            CalculatedBitcoinAverageAnnualReturnRate10Years.Content = $"{bitcoinAverageAnnualReturnRateFromBirth * 100:F2}%";
         }
 
         private void CalculateButton_Click(object sender, RoutedEventArgs e)
