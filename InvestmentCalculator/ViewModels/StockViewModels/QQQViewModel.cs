c594fc25efafb216a6632769ee2452ef2d00c65e
Trevor Bai <baijin2011@gmail.com>
2026-01-11 15:04:58 -0500

    Reset QQQ placeholder data as 0s

--- InvestmentCalculator/ViewModels/StockViewModels/QQQViewModel.cs
+++ InvestmentCalculator/ViewModels/StockViewModels/QQQViewModel.cs
@@ -4,14 +4,14 @@ namespace InvestmentCalculator.ViewModels.StockViewModels
 {
     internal class QQQViewModel : CAGRViewModel
     {
-        // TODO all the data are placeholder data, which are costco's.
-        private const double PriceAt2025Dec19th = 855.62;
+        // TODO all the data are placeholder data.
+        private const double PriceAt2025Dec19th = 0;
 
-        private const double PriceAt2020Dec21st = 364.58;
-        private const double PriceAt2021Dec20th = 550.37;
-        private const double PriceAt2022Dec19th = 462.65;
-        private const double PriceAt2023Dec18th = 671.60;
-        private const double PriceAt2024Dec16th = 954.07;
+        private const double PriceAt2020Dec21st = 0;
+        private const double PriceAt2021Dec20th = 0;
+        private const double PriceAt2022Dec19th = 0;
+        private const double PriceAt2023Dec18th = 0;
+        private const double PriceAt2024Dec16th = 0;
 
         internal QQQViewModel() : base(
             PriceAt2025Dec19th,
