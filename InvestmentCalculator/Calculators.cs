4ee66b128f93d59649935824782f82deefdfd9c5
Trevor Bai <baijin2011@gmail.com>
2026-01-11 15:03:50 -0500

    Handle edge case for CAGR calculator

--- InvestmentCalculator/Calculators.cs
+++ InvestmentCalculator/Calculators.cs
@@ -5,6 +5,7 @@
         internal static double CalculateAverageAnualReturnRate(double beginningValue,
             double endingValue, double yearSpan)
         {
+            if (beginningValue is 0) return 0;
             var totalGrowthFactor = endingValue / beginningValue;
             var compoundAnnualGrowthRate = Math.Pow(totalGrowthFactor, 1 / yearSpan) - 1;
             return compoundAnnualGrowthRate;
