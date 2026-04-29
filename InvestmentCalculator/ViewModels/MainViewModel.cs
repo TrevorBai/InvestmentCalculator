f6750d24ebe49fbcaf549d087e87266e9508fba4
Trevor Bai <baijin2011@gmail.com>
2026-04-28 23:42:52 -0400

    Clean up main view model

--- InvestmentCalculator/ViewModels/MainViewModel.cs
+++ InvestmentCalculator/ViewModels/MainViewModel.cs
@@ -258,59 +258,6 @@ namespace InvestmentCalculators.ViewModels
             return btcData;
         }
 
-
-        private static AssetData Get5YrsAssetDataFromDb(List<AssetPrice> assetPrices,
-            string assetTicker, DateTime anchorEndingDate, bool byAdjustedClose = false)
-        {
-            var startRangeDate = new DateTime(anchorEndingDate.Year - 5, anchorEndingDate.Month, 1);
-            var endRangeDate = new DateTime(anchorEndingDate.Year, anchorEndingDate.Month, 1)
-                .AddMonths(1).AddDays(-1);
-
-            var allPrices = GetPriceRange(assetPrices, assetTicker, startRangeDate, endRangeDate);
-
-            var date1YearAgo = anchorEndingDate.AddYears(-1);
-            var date2YearsAgo = anchorEndingDate.AddYears(-2);
-            var date3YearsAgo = anchorEndingDate.AddYears(-3);
-            var date4YearsAgo = anchorEndingDate.AddYears(-4);
-            var date5YearsAgo = anchorEndingDate.AddYears(-5);
-
-            var priceAtEndingDate = byAdjustedClose 
-                ? FindPriceByAdjClose(anchorEndingDate, allPrices)
-                : FindPriceByClose(anchorEndingDate, allPrices);
-            var priceAt1YearAgo = byAdjustedClose 
-                ? FindPriceByAdjClose(date1YearAgo, allPrices)
-                : FindPriceByClose(date1YearAgo, allPrices);
-            var priceAt2YearsAgo = byAdjustedClose 
-                ? FindPriceByAdjClose(date2YearsAgo, allPrices) 
-                : FindPriceByClose(date2YearsAgo, allPrices);
-            var priceAt3YearsAgo = byAdjustedClose 
-                ? FindPriceByAdjClose(date3YearsAgo, allPrices) 
-                : FindPriceByClose(date3YearsAgo, allPrices);
-            var priceAt4YearsAgo = byAdjustedClose 
-                ? FindPriceByAdjClose(date4YearsAgo, allPrices) 
-                : FindPriceByClose(date4YearsAgo, allPrices);
-            var priceAt5YearsAgo = byAdjustedClose 
-                ? FindPriceByAdjClose(date5YearsAgo, allPrices) 
-                : FindPriceByClose(date5YearsAgo, allPrices);
-
-            var assetData = new AssetData
-            {
-                EndPrice = (decimal)priceAtEndingDate,
-                EndDate = DateOnly.FromDateTime(anchorEndingDate),
-                Price1YearAgoFromEndDate = (decimal)priceAt1YearAgo,
-                Date1YearAgo = DateOnly.FromDateTime(date1YearAgo),
-                Price2YearsAgoFromEndDate = (decimal)priceAt2YearsAgo,
-                Date2YearsAgo = DateOnly.FromDateTime(date2YearsAgo),
-                Price3YearsAgoFromEndDate = (decimal)priceAt3YearsAgo,
-                Date3YearsAgo = DateOnly.FromDateTime(date3YearsAgo),
-                Price4YearsAgoFromEndDate = (decimal)priceAt4YearsAgo,
-                Date4YearsAgo = DateOnly.FromDateTime(date4YearsAgo),
-                Price5YearsAgoFromEndDate = (decimal)priceAt5YearsAgo,
-                Date5YearsAgo = DateOnly.FromDateTime(date5YearsAgo)
-            };
-            return assetData;
-        }
-
         /// <summary>
         /// If can't find the price on that specific date, find the most recent price
         /// before that date.
@@ -322,19 +269,6 @@ namespace InvestmentCalculators.ViewModels
                 .OrderByDescending(p => p.Date)            // Get the most recent one
                 .FirstOrDefault();
             return lastAvailable?.Close ?? 0;
-        }
-
-        /// <summary>
-        /// If can't find the price on that specific date, find the most recent price
-        /// before that date.
-        /// </summary>
-        private static double FindPriceByAdjClose(DateTime targetDate, List<AssetPrice> assetPrices)
-        {
-            var lastAvailable = assetPrices
-                .Where(p => p.Date.Date <= targetDate.Date)
-                .OrderByDescending(p => p.Date)            
-                .FirstOrDefault();
-            return lastAvailable?.AdjClose ?? 0;
         }
 
         private static List<AssetPrice> GetPriceRange(List<AssetPrice> assetPrices,
@@ -351,13 +285,6 @@ namespace InvestmentCalculators.ViewModels
             return await db.AssetPrices.AsNoTracking().ToListAsync();
         }
 
-
-
-
     }
 }
 
-//public class ReturnRatePeriod : INotifyPropertyChanged
-//{
-//    //public event PropertyChangedEventHandler? PropertyChanged;
-//}
